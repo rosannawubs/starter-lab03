57c9c75c4ff0d64d52b452fd890a3bdcc0677923
rwubs <rwubs@142.232.165.136>
2015-09-24 16:12:02 -0700

    the seventh page done

--- application/config/routes.php
+++ application/config/routes.php
@@ -44,6 +44,7 @@ $route['sleep'] = 'first/zzz';
 $route['lock/(:any)/(:any)'] = 'welcome/shucks';
 $route['show/(:num)'] = 'first/gimme/$1';
 $route['dunno'] = 'guess';
+$route['([a-zA-Z]{4})/bingo'] = 'bingo';
 
 
 /* End of file routes.php */
