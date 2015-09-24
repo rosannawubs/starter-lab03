82a0c95651bd7cc17800f67f7ced9aca54a77d7a
rwubs <rwubs@142.232.165.136>
2015-09-24 15:12:44 -0700

    the second page done

---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/Welcome.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class Welcome extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $author = $this->quotes->last();
+
+        $this->data['mug'] = $author['mug'];
+        $this->data['what'] = $author['what'];
+        $this->data['who'] = $author['who'];
+
+        $this->render();
+    }
+
+}
+
+/* End of file Welcome.php */
+/* Location: application/controllers/Welcome.php */
